aad0ef8bb923d8c8efa3dcfb11404f7ae50aafeb
Vadym <kirilkovadim193@gmail.com>
2025-09-08 07:21:39 +0300

    Refactor default permission classes in settings.py

--- cinema_service/settings.py
+++ cinema_service/settings.py
@@ -138,8 +138,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": [
+    "DEFAULT_AUTHENTICATION_CLASSES":
         "rest_framework.authentication.TokenAuthentication",
-        "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
-    ],
+    "DEFAULT_PERMISSION_CLASSES" :
+    "cinema.permissions.IsAdminOrIfAuthenticatedReadOnly"
 }
